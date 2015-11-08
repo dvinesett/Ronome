151f993c0a623e398446e300a93c610eff0492fa
dvinesett <dvinesett@gmail.com>
2015-11-08 14:50:34 -0500

    initial commit with 1 page view template

---
+++ Ronome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ronome
+//
+//  Created by Vinesett, Dylan on 11/8/15.
+//  Copyright © 2015 Vinesett, Dylan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
