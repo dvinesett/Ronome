fa685ff22d3102633d8372eaa8c01b4112b33074
dvinesett <dvinesett@gmail.com>
2015-11-15 19:02:15 -0500

    basic tempo working. still ugly view

--- Ronome/ViewController.swift
+++ Ronome/ViewController.swift
@@ -7,33 +7,75 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     
-    @IBOutlet weak var stepperTempo: UIStepper!
-    @IBOutlet weak var labelTempo: UILabel!
+    @IBOutlet weak var tempoLabel: UILabel!
+    @IBOutlet weak var tempoStepper: UIStepper!
     
-    var tempo: Int
     
-
-    @IBAction func changeTempo(stepperTempo: UIStepper) {
-        tempo = stepperTempo.value
+    var metronomeTimer: NSTimer!
+    
+    var metronomeIsOn = false
+    
+    var metronomeSoundPlayer: AVAudioPlayer!
+    
+    var tempo: NSTimeInterval = 20 {
+        didSet {
+            tempoLabel.text = String(format: "%.0f", tempo)
+            tempoStepper.value = Double(tempo)
+        }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func changeTempo(tempoStepper: UIStepper) {
+        tempo = tempoStepper.value
     }
-
+    
+    @IBAction func toggleClick(sender: UIButton) {
+        if metronomeIsOn {
+            metronomeIsOn = false
+            
+            metronomeTimer?.invalidate()
+            tempoStepper.enabled = true
+            
+            tempoLabel.enabled = true
+        } else {
+            metronomeIsOn = true
+            
+            let metronomeTimeInterval:NSTimeInterval = 60.0 / tempo
+            metronomeTimer = NSTimer.scheduledTimerWithTimeInterval(metronomeTimeInterval, target: self, selector: Selector("playMetronomeSound"), userInfo: nil, repeats: true)
+            metronomeTimer?.fire()
+            
+            tempoStepper.enabled = false
+            
+            tempoLabel.resignFirstResponder()
+            
+            tempoLabel.enabled = false
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func playMetronomeSound() {
+        let currentTime = CFAbsoluteTimeGetCurrent()
+        print("Play metronome sound @ \(currentTime)")
+        
+        metronomeSoundPlayer.play()
+    }
 
     override func viewDidLoad() {
-        super.didViewLoad()
+        super.viewDidLoad()
+        
+        tempo = 240
+        
+        let metronomeSoundURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("snizzap", ofType: "wav")!)
+        metronomeSoundPlayer = try? AVAudioPlayer(contentsOfURL: metronomeSoundURL)
+        metronomeSoundPlayer.prepareToPlay()
         
     }
 }
