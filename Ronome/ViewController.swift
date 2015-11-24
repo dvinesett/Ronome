8481ecdd107f38af66267c6c6604e972953ca12e
dvinesett <dvinesett@gmail.com>
2015-11-24 09:54:03 -0500

    accent and partial time signature

--- Ronome/ViewController.swift
+++ Ronome/ViewController.swift
@@ -15,10 +15,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var tempoLabel: UILabel!
     @IBOutlet weak var tempoStepper: UIStepper!
     @IBOutlet weak var tempoSlider: UISlider!
+    @IBOutlet weak var tsUpperLabel: UILabel!
+    @IBOutlet weak var tsUpperStepper: UIStepper!
+    @IBOutlet weak var tsLowerLabel: UILabel!
+    @IBOutlet weak var tsLowerStepper: UIStepper!
+    @IBOutlet weak var countLabel: UILabel!
     
     var metronomeTimer: NSTimer!
     var metronomeIsOn = false
     var metronomeSoundPlayer: AVAudioPlayer!
+    var metronomeAccentPlayer: AVAudioPlayer!
+    var count: Int = 0 {
+        didSet {
+            if count == 0 {
+                countLabel.text = String(tsUpper)
+            } else {
+                countLabel.text = String(count)
+            }
+        }
+    }
     var tempo: NSTimeInterval = 60 {
         didSet {
             tempoLabel.text = String(format: "%.0f", tempo)
@@ -26,10 +41,33 @@ class ViewController: UIViewController {
             tempoSlider.value = Float(tempo)
         }
     }
+    var tsUpper: Int = 4 {
+        didSet {
+            tsUpperLabel.text = String(tsUpper)
+            tsUpperStepper.value = Double(tsUpper)
+        }
+    }
+    var tsLower: Int = 4 {
+        didSet {
+            tsLowerLabel.text = String(tsLower)
+            tsLowerStepper.value = Double(tsLower)
+        }
+    }
     
+
+
     @IBAction func stepTempo(sender: UIStepper) {
         tempo = sender.value
     }
+    
+    @IBAction func stepTsUpper(sender: UIStepper) {
+        tsUpper = Int(sender.value)
+    }
+    
+    @IBAction func stepTsLower(sender: UIStepper) {
+        tsLower = Int(sender.value)
+    }
+    
     
     @IBAction func slideTempo(sender: UISlider) {
         tempo = Double(sender.value)
@@ -40,14 +78,18 @@ class ViewController: UIViewController {
             // stop
             metronomeIsOn = false
             metronomeTimer?.invalidate()
+            count = 0
             sender.setTitle("Start", forState: UIControlState.Normal)
         } else {
             // start
             metronomeIsOn = true
+            sender.setTitle("Stop", forState: UIControlState.Normal)
+            
             let metronomeTimeInterval:NSTimeInterval = 60.0 / tempo
             metronomeTimer = NSTimer.scheduledTimerWithTimeInterval(metronomeTimeInterval, target: self, selector: Selector("playMetronomeSound"), userInfo: nil, repeats: true)
+            
             metronomeTimer?.fire()
-            sender.setTitle("Stop", forState: UIControlState.Normal)
+            
         }
     }
     
@@ -57,19 +99,33 @@ class ViewController: UIViewController {
 
     func playMetronomeSound() {
         let currentTime = CFAbsoluteTimeGetCurrent()
-        print("Play metronome sound @ \(currentTime)")
-        
-        metronomeSoundPlayer.play()
+        print("\(count) Play metronome sound @ \(currentTime)")
+        count++
+        if count == 1 {
+            metronomeAccentPlayer.play()
+        } else {
+            metronomeSoundPlayer.play()
+            if count == tsUpper {
+                count = 0
+            }
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tempo = 60
+        tempo = 120
+        tsUpper = 4
+        tsLower = 4
         
-        let metronomeSoundURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("snizzap", ofType: "wav")!)
+        let metronomeSoundURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("snizzap1", ofType: "wav")!)
+        let metronomeAccentURL = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("snizzap2", ofType: "wav")!)
+
         metronomeSoundPlayer = try? AVAudioPlayer(contentsOfURL: metronomeSoundURL)
+        metronomeAccentPlayer = try? AVAudioPlayer(contentsOfURL: metronomeAccentURL)
+        
         metronomeSoundPlayer.prepareToPlay()
+        metronomeAccentPlayer.prepareToPlay()
     }
 }
 
